b944e4efae6f295a222c53666cca584a482530d7
Adelaide Nalley <adiainthesky@gmail.com>
2020-10-08 17:54:11 -0700

    make fully functional with li list items, capitalized, and in alphabetical order

--- js/scripts.js
+++ js/scripts.js
@@ -12,39 +12,14 @@ $(document).ready(function() {
       capitalGroceries.push(item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase());
     });
 
-    // const capitalGroceries = groceries.map(function(item) {
-    //   return item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase();
-    // });
     
-    
-    
-    $("li").text(capitalGroceries);
+    let string = "";
+    capitalGroceries.forEach(function(item){
+      string += '<li>' + item + '</li>';
+    });
+
+    string = '<ul>' + string + '</ul>';
+    console.log(string);
+    document.querySelector("#list").innerHTML = string;
   });   
 });
-
-// const restLower = items.slice(1,items.length).toLowerCase();
-// const capital = firstUpper + restLower;
-    // const inputName = $("input#name").val();
-    // const firstUpper = inputName.slice(0,1).toUpperCase();
-    // const restLower = inputName.slice(1,inputName.length).toLowerCase();
-    // const readyName = firstUpper + restLower;
-
-
-
- // works but make a long string of items
-// $(document).ready(function() {
-//   const groceries = [];
-//   $("form#form-group").submit(function(event) {
-//     event.preventDefault();
-//     groceries.push($("input#item").val());
-   
-//     console.log(groceries);
-    
-//     groceries.forEach(function(item) {
-     
-//       $("li").text(groceries);
-      
-   
-//     });
-//   });  
-// });
